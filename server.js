b8741f0e621c51d363132527f578096d0e4c9c94
Mosope-Adeyemi-dev <78312219+Mosope-Adeyemi-dev@users.noreply.github.com>
2022-04-21 15:22:25 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 
 const app = express();
