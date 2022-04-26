4fd3af73d51b52466bdf0b1045e43d79c2b6593d
Daniel Komolafe <komozy2000@gmail.com>
2022-04-26 19:50:44 +0100

    added cors middleware

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const { readdirSync } = require("fs");
 
 const app = express();
 
 // middlewares
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
